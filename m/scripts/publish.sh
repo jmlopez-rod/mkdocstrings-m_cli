7e9ce9b706646fa98546e52abcaf56365998fbd3
Manuel Lopez <jmlopez.rod@gmail.com>
2023-11-06 20:10:14 +0000

    fix publish script

--- m/scripts/publish.sh
+++ m/scripts/publish.sh
@@ -13,7 +13,7 @@ set -xeuo pipefail
 python3 -m twine upload .stage-pypi/dist/*
 
 # Let github know we released
-# m github release --owner jmlopez-rod --repo mkdocstrings-m_cli--version "$M_TAG"
+m github release --owner jmlopez-rod --repo mkdocstrings-m_cli --version "$M_TAG"
 
 # Create v tags
-# m git tag_release --version "$M_TAG" --major-only
+m git tag_release --version "$M_TAG" --major-only
